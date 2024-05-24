e4b66816d9729405ecf4be5f9aec59ea20429ee3
williamivan <s32210112@student.ubm.ac.id>
2024-05-24 23:05:41 +0700

    portofolio website-first commit

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*remove menu*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    emailjs.sendForm('service_d3psfpo', 'template_8gaj7n6', '#contact-form', 'wgaAlEmLwQYo9EXDW')
+        .then(()=>{
+            contactMessage.textContent = 'Message Sent Successfully'
+
+            setTimeout(()=>{
+                contactMessage.textContent = ''
+            }, 5000)
+
+            contactForm.reset()
+
+        }, ()=>{
+            contactMessage.textContent = 'Message Not Sent (service error)'
+        })
+}
+
+contactForm.addEventListener('submit', sendEmail)
